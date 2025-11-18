efdc5c50c449a98e6008f4e7063349d0c3443479
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2025-11-18 12:39:57 +0100

    zoom

--- src/poiupv/FXMLDocumentController.java
+++ src/poiupv/FXMLDocumentController.java
@@ -141,7 +141,7 @@ public class FXMLDocumentController implements Initializable {
         initData();
         //==========================================================
         // inicializamos el slider y enlazamos con el zoom
-        zoom_slider.setMin(0.5);
+        zoom_slider.setMin(0.1);
         zoom_slider.setMax(1.5);
         zoom_slider.setValue(1.0);
         zoom_slider.valueProperty().addListener((o, oldVal, newVal) -> zoom((Double) newVal));
